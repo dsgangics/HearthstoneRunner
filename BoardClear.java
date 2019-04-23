d32142ca2c7bf3d6fe8dd614987fc86848055b52
Stevhan <33045659+Stevhan@users.noreply.github.com>
2019-04-23 14:34:26 -0400

    Update BoardClear.java

--- BoardClear.java
+++ BoardClear.java
@@ -1,5 +1,12 @@
-package hearthstone;
+package hearthstrone;
 
-public class BoardClear extends Puzzles{
+public class BoardClear extends Puzzles {
 
+    public BoardClear(Card[] d, Card[] ed, Card[] h, Card[] eh, Card[] fc, Card[] efc, int sm, int cm, Hero aHero, Hero eHero) {
+        super(d, ed, h, eh, fc, efc, sm, cm, aHero, eHero);
+    }
+
+    public boolean checkWinCondition() {//checks all enemy minions have been removed
+        return true;
+    }
 }
