4c03cf68c182d717048631964a5408dd1e694940
Stevhan <33045659+Stevhan@users.noreply.github.com>
2019-04-23 14:44:57 -0400

    Update Warrior.java

--- Warrior.java
+++ Warrior.java
@@ -1,13 +1,14 @@
 package hearthstone;
 
-public class Rogue extends Hero{
-    
-    public Rogue(int h, String hp){
+public class Warrior extends Hero{
+
+    //constructor
+    public Warrior(int h, String hp){
         super(h, hp);
     }
     
-    public int[] heroPower(){//Hero 0
-        int arr[] = {0};
+    public int[] heroPower(){//Hero 8, Amour 2
+        int arr[] = {8, 2};
         return arr;       
     }
 }
