fc61c2d21a816b6d33a87b2c501bcb63f908c0ab
Stevhan <33045659+Stevhan@users.noreply.github.com>
2019-04-23 14:38:05 -0400

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,18 +1,22 @@
-package hearthstrone;
+package hearthstone;
 
 abstract class Hero {
-    
+
     int health;
     String heroPower;
-    
+
     //constructor
-    public Hero(int h, String hp){
+    public Hero(int h, String hp) {
         health = h;
         heroPower = hp;
     }
-    
+
     //each hero has a unique ability
     abstract int[] heroPower();
-    
+
+    //set the starting hero's hp through each puzzle
+    public void setHeroHP(int hp) {
+        health = hp;
+    }
 
 }
