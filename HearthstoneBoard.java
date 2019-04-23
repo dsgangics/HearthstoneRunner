5755a236af91c39246fd48624be58bda5705631e
Stevhan <33045659+Stevhan@users.noreply.github.com>
2019-04-23 14:37:09 -0400

    Update HearthstoneBoard.java

--- HearthstoneBoard.java
+++ HearthstoneBoard.java
@@ -1,5 +1,19 @@
 package hearthstone;
 
-public class HearthstoneBoard extends HearthstoneRunner{
+public class HearthstoneBoard extends HearthstoneRunner {
 
+    Card deck[];
+    Card enemyDeck[];
+    Card hand[] = new Card [10];
+    Card enemyHand[] = new Card [10];
+    Card fieldCards[] = new Card [7];
+    Card enemyFieldCards[] = new Card [7];
+    int startMana;
+    int currentMana;
+    Hero allyHero;
+    Hero enemyHero;
+
+    public HearthstoneBoard() {
+
+    }
 }
