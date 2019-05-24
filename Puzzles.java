a2fc945d33d13ede76ec73c6e8909d38a3fc2062
Stevhan <33045659+Stevhan@users.noreply.github.com>
2019-05-24 14:38:49 -0400

    Update Puzzles.java

--- Puzzles.java
+++ Puzzles.java
@@ -4,20 +4,7 @@ abstract public class Puzzles{
     protected Puzzles levels[][] = new Puzzles[4][7];
     
     public Puzzles(){
-        for(int i = 0; i < 4; i++){
-            for(int a = 0; a < 7; a++){
-                if(i == 0){
-                    levels[i][a] = new Lethal();
-                }else if(i == 1){
-                    levels[i][a] = new Mirror();
-                }else if(i == 2){
-                    levels[i][a] = new BoardClear();
-                }else if(i == 3){
-                    levels[i][a] = new Survival();
-                }
-            }
-        }
-    }
-    
+        
+    }   
     
 }
